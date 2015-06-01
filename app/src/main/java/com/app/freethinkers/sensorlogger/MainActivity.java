fb00bfda73b3d6837e720ef9cfd5a7814d18d9e2
Patrick Keil <trancefusion@gno.de>
2015-06-01 10:37:07 +0200

    Init Rev.

---
+++ app/src/main/java/com/app/freethinkers/sensorlogger/MainActivity.java
@@ -0,0 +1,47 @@
+package com.app.freethinkers.sensorlogger;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            //Start Settings Activity
+            viewSettings();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void viewSettings() {
+        Intent intent = new Intent(this, SettingsLessActivity.class);
+        startActivity(intent);
+    }
+}
