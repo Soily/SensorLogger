757d9c01b7013d3b006e4366624b7f0e8aa56ed2
Patrick Keil <trancefusion@gno.de>
2015-06-01 10:36:43 +0200

    Init Rev.

---
+++ app/src/main/java/com/app/freethinkers/sensorlogger/SettingsLessActivity.java
@@ -0,0 +1,79 @@
+package com.app.freethinkers.sensorlogger;
+
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+
+
+public class SettingsLessActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_less);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onRadioButtonsClicked(View v){
+
+        // Set all Radio Buttons to false
+        int RadioButton = v.getId();
+
+        RadioButton Button1 = (RadioButton) findViewById(R.id.radioButton);
+        RadioButton Button2 = (RadioButton) findViewById(R.id.radioButton2);
+        RadioButton Button3 = (RadioButton) findViewById(R.id.radioButton3);
+        RadioButton Button4 = (RadioButton) findViewById(R.id.radioButton4);
+        RadioButton Button5 = (RadioButton) findViewById(R.id.radioButton5);
+
+        Button1.setChecked(false);
+        Button2.setChecked(false);
+        Button3.setChecked(false);
+        Button4.setChecked(false);
+        Button5.setChecked(false);
+
+        RadioButton myButton = (RadioButton) findViewById(RadioButton);
+        myButton.setChecked(true);
+
+        switch(RadioButton) {
+            case R.id.radioButton:
+
+                break;
+            case R.id.radioButton2:
+
+                break;
+            case R.id.radioButton3:
+
+                break;
+            case R.id.radioButton4:
+
+                break;
+            case R.id.radioButton5:
+
+                break;
+        }
+    }
+}
